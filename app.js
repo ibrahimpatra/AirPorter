af7989ad031c038acf1c720e62683c6642c6f746
Ibrahim Patrawala <ibupatra@gmail.com>
2021-09-20 19:42:24 +0300

    ready again

--- app.js
+++ app.js
@@ -124,6 +124,8 @@ app.use((err, req, res, next) => {
 
 })
 
-app.listen(3000, ()=> {
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
      console.log('Working on Port 3000')
  })
